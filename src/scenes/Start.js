2bf8b92055bc2c0a0920b59f322859faefe1803a
adrianooliveira37 <48990391+adrianooliveira37@users.noreply.github.com>
2025-03-21 20:51:40 -0300

    Update Start.js

--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -30,16 +30,16 @@ export class Start extends Phaser.Scene {
         this.ship.play('fly');
 
         // Controles touch para dispositivos móveis
-        this.upButton = this.add.sprite(100, 600, 'upButton').setInteractive().setScale(0.8);
-        this.downButton = this.add.sprite(100, 700, 'downButton').setInteractive().setScale(0.8);
-        this.shootButton = this.add.sprite(1180, 600, 'shootButton').setInteractive().setScale(0.8);
+        this.upButton = this.add.sprite(100, 600, 'upButton').setInteractive().setScale(0.30);
+        this.downButton = this.add.sprite(100, 700, 'downButton').setInteractive().setScale(0.30);
+        this.shootButton = this.add.sprite(1180, 600, 'shootButton').setInteractive().setScale(0.30);
 
         this.upButton.on('pointerdown', () => {
-            this.ship.y -= 5;
+            this.ship.y -= 15;
         });
 
         this.downButton.on('pointerdown', () => {
-            this.ship.y += 5;
+            this.ship.y += 15;
         });
 
         this.shootButton.on('pointerdown', () => {
